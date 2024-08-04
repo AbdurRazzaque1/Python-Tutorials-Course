d0f77f7c2fd57cf68f080691470ddcd7b2103839
Khan Abdul Razzaque <145262445+Khan-Abdul-Razzaque@users.noreply.github.com>
2024-08-04 01:41:25 +0000

    modified

--- vehical_parking_problem.py
+++ vehical_parking_problem.py
@@ -1,74 +1,80 @@
 "Vehical Parking Problem"
 
-print("Press 1 to park Or press -1 to remove a Riksha.")
+print("Press 1 to park Or press -1 to remove a Rikshaw.")
 print("Press 2 to park Or press -2 to remove a Car.")
 print("Press 3 to park Or press -3 to remove a Bus.")
 print("Press 4 to erase all the data.")
 print("Press 5 to see record.")
 print("press 0 to quit the program.\n")
-C = 0
-D = 0
-E = 0
-F = 0
-A = 0
+totalVehicles = 0
+rikshaws = 0
+cars = 0
+buses = 0
+totalAmount = 0
 while True:
-    B = int(input("Press your choise: "))
-    if (-3 <= B <= 5 and B != 0):
-        if C < 50:
-            if B == 1:
-                A += 100
-                D += 1
-            elif B == -1:
-                if D > 0:
-                    D -= 1
+    choice = int(input("Press your choise: "))
+    if (-3 <= choice <= 5 and choice != 0):
+        if totalVehicles < 50:
+            if choice == 1:
+                totalAmount += 100
+                rikshaws += 1
+            elif choice == -1:
+                if rikshaws > 0:
+                    rikshaws -= 1
                 else:
-                    print("Sorry, No Rikshaw is present in the parking")
-            elif B == 2:
-                A += 200
-                E += 1
-            elif B == -2:
-                if E > 0:
-                    E -= 1
+                    print("\n*** Sorry, No Rikshaw is present in the parking ***\n")
+            elif choice == 2:
+                totalAmount += 200
+                cars += 1
+            elif choice == -2:
+                if cars > 0:
+                    cars -= 1
                 else:
-                    print("Sorry, No Car is present in the parking")
-            elif B == 3:
-                A += 300
-                F += 1
-            elif B == -3:
-                if F > 0:
-                    F -= 1
+                    print("\n*** Sorry, No Car is present in the parking ***\n")
+            elif choice == 3:
+                totalAmount += 300
+                buses += 1
+            elif choice == -3:
+                if buses > 0:
+                    buses -= 1
                 else:
-                    print("Sorry, No Bus is present in the parking")
+                    print("\n*** Sorry, No Bus is present in the parking ***\n")
                     
-            elif B == 4:
-                A = 0
-                C = 0
-                D = 0
-                E = 0
-                F = 0
-            elif B == 5:
-                print(f"The number of Rikshaws is {D}")
-                print(f"The number of Cars is {E}")
-                print(f"The number of Buses is {F}")
-                print(f"The tatal number of vehical in the parking is {C} and total amount is ₹{A}")
-        elif B == 4:
-            A = 0
-            C = 0
-            D = 0
-            E = 0
-            F = 0
-        elif B == 5:
-            print(f"The number of Rikshaws is {D}")
-            print(f"The number of Cars is {E}")
-            print(f"The number of Busses is {F}")
-            print(f"The tatal number of vehical in the parking is {C} and total amount is ₹{A}")
+            elif choice == 4:
+                print("\n#! All the data has been removed from the parking record !#\n")
+                totalAmount = 0
+                totalVehicles = 0
+                rikshaws = 0
+                cars = 0
+                buses = 0
+            elif choice == 5:
+                print("\n#!**************************!#")
+                print(f"The number of Rikshaws is {rikshaws}")
+                print(f"The number of Cars is {cars}")
+                print(f"The number of Buses is {buses}")
+                print(f"The tatal number of vehical in the parking is {totalVehicles} and total amount is ₹{totalAmount}")
+                print("#!**************************!#\n")
+        elif choice == 4:
+
+            print("\n#! All the data has been removed from the parking record !#\n")
+            totalAmount = 0
+            totalVehicles = 0
+            rikshaws = 0
+            cars = 0
+            buses = 0
+        elif choice == 5:
+            print("\n#!**************************!#")
+            print(f"The number of Rikshaws is {rikshaws}")
+            print(f"The number of Cars is {cars}")
+            print(f"The number of Busses is {buses}")
+            print(f"The tatal number of vehical in the parking is {totalVehicles} and total amount is ₹{totalAmount}")
+            print("#!**************************!#\n")
         else:
-            print("Sorry the parking is full.", end= " ")
-            print("Remove a vehicle from the parking and then park a new vehicle.")
-    elif B == 0:
-        print("THANK YOU SO MUCH FOR USING THIS PROGRAM")
+            print("\n#? Sorry the parking is full,", end= " ")
+            print("Remove a vehicle OR Erase all the data to park new vehicles #?.\n")
+    elif choice == 0:
+        print("\n*** THANK YOU SO MUCH FOR USING THIS PROGRAM ***\n")
         break
     else:
-        print("Something went wrong")
-    C = D + E + F
-# ! this is a comment line and it is going to push on the Github.
+        print("\n#!!!! Something went wrong !!!!#\n")
+    totalVehicles = rikshaws + cars + buses
